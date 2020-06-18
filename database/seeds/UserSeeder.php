44f57448b96d45ca7971403d245f66b48926d89c
AGodinez <ing.a.godinez@gmail.com>
2020-06-18 11:09:50 -0500

    add some changes on seeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -2,6 +2,7 @@
 
 use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -12,9 +13,12 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        factory(User::class)->create([
+        User::create([
             'name' => 'Andres Godinez',
-            'email' => 'ing.a.godinez@gmail.com'
+            'email' => 'ing.a.godinez@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
         ]);
 
     }
